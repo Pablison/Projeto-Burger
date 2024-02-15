fcb1de50a8513e18b286ef13225846af97221f48
Pablison <pablisonviana@gmail.com>
2024-02-15 15:52:49 -0300

    criação das primeiras rotas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="nav">
+        <RouterLink to="/">
+          <img src="" id="logo">
+        </RouterLink>
+
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </div>
+
+</template>
+<script>
+  export default {
+    name: "Navbar"
+  }
+</script>
+<style scoped>
+
+</style>
+
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>PEDIDOS</h1>
+  </div>
+</template>
+
+<style>
+</style>
